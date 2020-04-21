dde5f8f42b8e61a4d5b2af1ab7d7f059cf001e28
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-04-20 21:19:47 -0700

    email script

---
+++ mail.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#
+#   This script interacts with an API in order to upload client reviews on
+#   the client's website. API is implemented using Django.
+#   
+#
+#   Author:     Sandro Aguilar
+#   Date:       April 19, 2020
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
